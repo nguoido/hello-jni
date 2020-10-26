eb67995bafa8fa4daf5428c45c018b1f867760b4
Do Khai <Khai.Do@windriver.com>
2020-10-26 15:03:18 +0700

    first commit

--- app/src/main/cpp/native-lib.c
+++ app/src/main/cpp/native-lib.c
@@ -5,8 +5,7 @@
 // Created by dkhai on 26/10/2020.
 //
 
-jstring Java_com_dkhai_myapplication_MainActivity_stringFromJNI( JNIEnv* env,
-                                                                 jobject thiz )
+jstring Java_com_dkhai_myapplication_MainActivity_stringFromJNI( JNIEnv* env, jobject thiz )
 {
     return (*env)->NewStringUTF(env, "Hello from C");
 }
